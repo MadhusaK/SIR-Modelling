269acf7b604be2693ce5812d5bb142eafd5d49a4
Madhusa <MadhusaK@gmail.com>
2014-09-28 23:06:34 +1000

    More updates

--- SIR - Vaccination Model (No Deaths or Births).py
+++ SIR - Vaccination Model (No Deaths or Births).py
@@ -19,8 +19,19 @@ I0 = 0.01
 R0 = 0.00
 initial_pop = (S0, I0, R0)
 
+x_max = 1
+x_min = 0
+y_max = 1
+y_min = 0
+points = 20
+x = numpy.linspace(x_min,x_max,points)
+y = numpy.linspace(y_min,y_max,points)
+X,Y = numpy.meshgrid(x,y)
+S_vecField = numpy.zeros(len(X))
+R_vecField = numpy.zeros(len(Y))
+
 # System of equations - S,I, and R
-def diff_SIR(SIR,t):
+def diff_SIR(SIR,t=0):
     ''' ODE's for S, I, R with Vaccinations '''
     
     output_SIR = numpy.zeros(3)
@@ -39,6 +50,7 @@ def diff_SIR(SIR,t):
 
 sol = scipy.integrate.odeint(diff_SIR,initial_pop,t_rng)
 
+pyplot.figure(1)
 pyplot.plot(sol[:,0],'-g',label='Susceptibles')
 pyplot.plot(sol[:,1],'-b',label='Infected')
 pyplot.plot(sol[:,2],'-r',label='Recovered')
@@ -53,6 +65,18 @@ print "Susceptible: %s" % sol[-1,0]
 print "Infected: %s" %sol[-1,1]
 print "Recovered: %s" % sol[-1,2]
 
+# Plotting Vector Field
+for i in range(0,len(X)):
+    sol_Prime = diff_SIR(X[1],Y[1])
+    S_vecField = sol_Prime[0]
+    R_vecField = sol_Prime[1]
+    
+pyplot.figure(2)
+pyplot.quiver(X,Y,S_vecField,R_vecField)   
+pyplot.hold()
+pyplot.plot(sol[:,0],sol[:,1])
+pyplot.axis(     )
+pyplot.hold()
 
 
 
